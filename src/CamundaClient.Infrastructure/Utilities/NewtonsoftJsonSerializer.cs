8b2df206f2cd8aeff5056b8ce2c2629e2050576c
Kostas Filokostas <filokostas@outlook.com>
2024-11-20 00:07:52 +0200

    Switch to Microsoft.Extensions.Http.Resilience
    Replaced Polly with Microsoft.Extensions.Http.Resilience for HTTP resilience handling. Updated CamundaClientOptions to remove resilience properties, now encapsulated in new ResilienceOptions class. Refactored AddCamundaHttpClient method. Updated test project dependencies and reformatted code for readability.

--- src/CamundaClient.Infrastructure/Utilities/NewtonsoftJsonSerializer.cs
+++ src/CamundaClient.Infrastructure/Utilities/NewtonsoftJsonSerializer.cs
@@ -5,20 +5,20 @@ using Newtonsoft.Json.Serialization;
 namespace CamundaClient.Infrastructure.Utilities;
 public class NewtonsoftJsonSerializer : IJsonSerializer
 {
-    private static readonly JsonSerializerSettings DefaultSettings = new JsonSerializerSettings
-    {
-        ContractResolver = new CamelCasePropertyNamesContractResolver(),
-        NullValueHandling = NullValueHandling.Ignore,
-        Formatting = Formatting.Indented
-    };
+	private static readonly JsonSerializerSettings DefaultSettings = new JsonSerializerSettings
+	{
+		ContractResolver = new CamelCasePropertyNamesContractResolver(),
+		NullValueHandling = NullValueHandling.Ignore,
+		Formatting = Formatting.Indented
+	};
 
-    public string Serialize<T>(T obj)
-    {
-        return JsonConvert.SerializeObject(obj, DefaultSettings);
-    }
+	public string Serialize<T>(T obj)
+	{
+		return JsonConvert.SerializeObject(obj, DefaultSettings);
+	}
 
-    public T Deserialize<T>(string json)
-    {
-        return JsonConvert.DeserializeObject<T>(json, DefaultSettings);
-    }
+	public T Deserialize<T>(string json)
+	{
+		return JsonConvert.DeserializeObject<T>(json, DefaultSettings)!;
+	}
 }
